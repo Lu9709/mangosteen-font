c2e468fe3b7278f46825cb6243ae2eae28f7783f
baizhe <919041098@qq.com>
2022-08-01 17:27:50 +0800

    feature:创建欢迎界面的页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,22 @@
+import { RouteRecordRaw } from 'vue-router'
+import { First } from '../components/welcome/first'
+import { Forth } from '../components/welcome/forth'
+import { Second } from '../components/welcome/second'
+import { Third } from '../components/welcome/third'
 import { Bar } from '../views/Bar'
 import { Foo } from '../views/Foo'
-export const routes = [
-    { path: '/', component: Foo },
-    { path: '/about', component: Bar }
+import { Welcome } from '../views/Welcome'
+export const routes: RouteRecordRaw[] = [
+  { path: '/', component: Foo },
+  { path: '/about', component: Bar },
+  {
+    path: '/welcome',
+    component: Welcome,
+    children: [
+      { path: '1', component: First },
+      { path: '2', component: Second },
+      { path: '3', component: Third },
+      { path: '4', component: Forth }
+    ]
+  }
 ]
