435d09885b203f0a41f61ceb7c9bea4d2632c4ae
baizhe <919041098@qq.com>
2022-08-03 21:09:29 +0800

    提取WelcomeLayout组件

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,21 +1,19 @@
-import { defineComponent } from 'vue';
 import s from './First.module.scss';
 import pig from '../../assets/icons/pig.svg';
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
+import { defineComponent } from 'vue';
 export const First = defineComponent({
   setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.pig} src={pig} />
-          <h2>会挣钱<br/>还要会省钱</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          <RouterLink to="/welcome/2" >下一页</RouterLink>
-          <RouterLink to="/start" >跳过</RouterLink>
-        </div>
-      </div>
-    )
+    const slots = {
+      icon: () => <img class={s.pig} src={pig} />,
+      title: () => <h2>会挣钱<br />还要会省钱</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+        <RouterLink to="/welcome/2" >下一页</RouterLink>
+        <RouterLink to="/start" >跳过</RouterLink>
+      </>
+    }
+    return () => <WelcomeLayout v-slots={slots}/>
   }
 })
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue'
+import s from './First.module.scss';
+import chart from '../../assets/icons/chart.svg'
+import { RouterLink } from 'vue-router'
+import { WelcomeLayout } from './WelcomeLayout'
 export const Third = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>Third</div>
+      <WelcomeLayout>
+      {{
+        icon: () => <img class={s.icon} src={chart} />,
+        title: () => <h2>每日提醒<br />不遗漏每一笔账单</h2>,
+        buttons: () => <>
+          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+          <RouterLink to="/welcome/4" >下一页</RouterLink>
+          <RouterLink to="/start" >跳过</RouterLink>
+        </>
+      }}
+    </WelcomeLayout>
     )
   }
 })
