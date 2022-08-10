0b0fbdd8d8c08efd1df0548a66a0ec7a0fcf430b
baizhe <919041098@qq.com>
2022-08-10 16:49:43 +0800

    完成四个欢迎卡片的切换

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,19 +1,34 @@
 import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue'
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router'
+import { RouteLocationNormalizedLoaded, RouterView, useRoute, useRouter } from 'vue-router'
 import s from './Welcome.module.scss'
 import { useSwipe } from '../hooks/useSwipe'
+import { throttle } from '../shared/throttle'
+const pushMap: Record<string, string> = {
+	'Welcome1': '/welcome/2',
+	'Welcome2': '/welcome/3',
+	'Welcome3': '/welcome/4',
+	'Welcome4': '/start',
+}
 export const Welcome = defineComponent({
 	setup: (props, context) => {
 		const main = ref<HTMLElement>()
-    // const { direction, swiping } = useSwipe(main)
-    // watchEffect(() => {
-    //   console.log(swiping.value, direction.value)
-    // })
+		const { direction, swiping } = useSwipe(main, { beforeStart: e => e.preventDefault() })
+		const route = useRoute()
+		const router = useRouter()
+		const push = throttle(() => {
+			const name = (route.name || 'Welcome1').toString()
+			router.push(pushMap[name])
+		}, 500)
+		watchEffect(() => {
+			if (swiping.value && direction.value === 'left') {
+				push()
+			}
+		})
 		return () =>
 			<div class={s.wrapper}>
 				<header>
 					<svg>
-						<use xlinkHref='#mangosteen'/>
+						<use xlinkHref='#mangosteen' />
 					</svg>
 					<h1>山竹记账</h1>
 				</header>
