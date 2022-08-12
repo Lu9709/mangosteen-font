d2c3e6e7fd5cc225a8fcafa47211a2d0574e36e9
baizhe <919041098@qq.com>
2022-08-12 16:18:08 +0800

    添加Icon组件

---
+++ src/shared/Icon.tsx
@@ -0,0 +1,19 @@
+import { defineComponent, PropType } from 'vue'
+import s from './Icon.module.scss'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig'
+
+export const Icon = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<IconName>,
+      required: true
+    }
+  },
+  setup: (props, context) => {
+    return () => (
+      <svg class={s.icon}>
+        <use xlinkHref={'#' + props.name}/>
+      </svg>
+    )
+  }
+})
