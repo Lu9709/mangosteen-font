5024a719eb22a03d1bbd1a6a4095603a9ca17503
baizhe <919041098@qq.com>
2022-08-22 16:40:36 +0800

    解决App.scss样式被覆盖的bug

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
+import { routes } from './config/routes'
 import { createApp } from 'vue'
 import { App } from './App'
-import { routes } from './config/routes'
 import { history } from './shared/history'
 import { createRouter } from 'vue-router'
 import '@svgstore'
 
 const router = createRouter({ history, routes })
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
