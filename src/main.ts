98908c88868200a7cf7fba719f78c307a5a3ed30
baizhe <919041098@qq.com>
2022-09-09 16:48:59 +0800

    登录检查

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,34 @@ import { App } from './App'
 import { history } from './shared/history'
 import { createRouter } from 'vue-router'
 import '@svgstore'
+import { mePromise, fetchMe } from './shared/me'
 
 const router = createRouter({ history, routes })
+
+fetchMe()
+
+const whiteList: Record<string, 'exact' | 'startsWith'> = {
+  '/': 'exact',
+  '/start': 'exact',
+  '/welcome': 'startsWith',
+  '/sign_in': 'startsWith' 
+}
+
+router.beforeEach((to, from) => {
+  for(const key in whiteList) {
+    const value = whiteList[key]
+    if(value === 'exact' && to.path === key) {
+      return true
+    }
+    if(value ==='startsWith' && to.path.startsWith(key)) {
+      return true
+    }
+  }
+  return mePromise!.then(
+    () => true,
+    () => '/sign_in?return_to=' + to.path
+  )
+})
 
 const app = createApp(App)
 app.use(router)
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -6,6 +6,7 @@ import { Button } from '../shared/Button'
 import { Form, FormItem } from '../shared/Form'
 import { http } from '../shared/Http'
 import { Icon } from '../shared/Icon'
+import { refreshMe } from '../shared/me'
 import { hasError, validate } from '../shared/validate'
 import s from './SignInPage.module.scss'
 export const SignInPage = defineComponent({
@@ -43,6 +44,7 @@ export const SignInPage = defineComponent({
         // 2. 拿去路由内的参数用于return_to跳转
         // 只需要其他地方使用 router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
         const returnTo = route.query.return_to?.toString()
+        refreshMe()
         router.push(returnTo ||  '/')
       }
     }
